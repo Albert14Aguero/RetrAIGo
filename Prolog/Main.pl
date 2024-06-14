dc799753069fdc01372b9840cfec532b6099ad22
Alberto <albert14aguero@gmail.com>
2024-06-14 15:26:29 -0600

    Heap

--- Prolog/Main.pl
+++ Prolog/Main.pl
@@ -1,16 +1,5 @@
-/*
-	[[1,2,e],
-	[8,2,4],
-	[7,6,4]]
+:- use_module(library(heaps)).
 
---board_from-->
-
-bord_row(1, [1,3,e]).
-bord_row(2, [8,2,4]).
-bord_row(3, [7,6,5]).
-
-predicados dinamicos
-*/
 %%%%%%%%%%%%%%%%% UTILS %%%%%%%%%%%%%%%%%%%%%%
 zip(L, M, Z) :- maplist( [X, Y, [X, Y]] >> true, L, M, Z)
 .
@@ -49,6 +38,90 @@ swap(L1, L2, Position, Position2, R3, R4):-
 	nth1(Position2, R3, E2, R1),
 	nth1(Position, R4, E1, R2)
 .
+
+board_from(L, Id) :-
+    board_new_id(Id),
+    board_clear(Id),
+    enumerate(L, EL),
+    forall(member(R, EL), board_add(Id, R))
+.
+board_list(Id, L):-
+    findall(Row, board_row(Id, _, Row), L)
+.
+board_to(Id, LS) :-
+	findall([I, Row], board_row(Id, I, Row), L),
+	sort(L, LS)
+.
+
+%%%%%%%%%%%%%% Calculations %%%%%%%%%%%%%%%%%
+
+find_position(Matrix, Number, Row, Col) :-
+    nth1(Row, Matrix, RowList),
+    nth1(Col, RowList, Number), !.
+
+
+manhattan_distance(List1, List2, Result):-
+	findall(R, (between(1, 8, X), manhattan_calculation(List1, List2, X, R)), ResultList),
+    sumlist(ResultList, Result)
+.
+
+manhattan_calculation(List1, List2, Number, Result):-
+	find_position(List1, Number, Row1, Col1),
+	find_position(List2, Number, Row2, Col2),
+	X is Row1 - Row2,
+	Y is Col1 - Col2,
+	abs(X, R1),
+	abs(Y, R2),
+ 	Result is R1 + R2
+.
+
+difference_distance(List1, List2, Result):-
+	list_of_lists_difference(List1, List2, Result)
+.
+
+list_difference([], [], 0).
+list_difference([], [_|_], 1).
+list_difference([_|_], [], 1).
+
+list_difference([H1|T1], [H2|T2], Count) :-
+    H1 == H2,
+    list_difference(T1, T2, CountRest),
+    Count is CountRest
+.
+
+list_difference([H1|T1], [H2|T2], Count) :-
+    H1 \== H2,
+    list_difference(T1, T2, CountRest),
+    Count is CountRest + 1
+.
+
+list_of_lists_difference([], [], 0).
+list_of_lists_difference([L1|T1], [L2|T2], Count) :-
+    list_difference(L1, L2, Diff),
+    list_of_lists_difference(T1, T2, DiffRest),
+    Count is Diff + DiffRest
+.
+list_of_lists_difference([L1|T1], [L2|T2], Count) :-
+    list_difference(L1, L2, Diff),
+    list_of_lists_difference(T1, T2, DiffRest),  
+    Count is Diff + DiffRest
+.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+priority_calculation(Id, P):-
+    board_list(Id, L1),
+    obtener_de_diccionario('resultado', IdR), 
+    board_list(IdR, L2),
+    manhattan_distance(L1, L2, P)
+.
+enqueue_board(HeapIn, IdBoard, Move, HeapOut):-
+    priority_calculation(IdBoard, P),
+    add_to_heap(HeapIn, P, [IdBoard, Move], HeapOut)
+.
+dequeue_board(HeapIn, IdBoard, Move, HeapOut):-
+    get_from_heap(HeapIn, _, [IdBoard, Move], HeapOut)
+.
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 :- dynamic visited/1.
 visited_clear :-
@@ -78,20 +151,13 @@ obtener_de_diccionario(Hash, IdBoard) :-
 eliminar_diccionario :-
     nb_delete(diccionario_global)
 .
+empty_priority_queue(Heap) :-
+    empty_heap(Heap).
 
+is_heap_empty(Heap) :-
+    heap_size(Heap, 0).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-board_from(L, Id) :-
-    board_new_id(Id),
-    board_clear(Id),
-    enumerate(L, EL),
-    forall(member(R, EL), board_add(Id, R))
-.
-
-board_to(Id, LS) :-
-	findall([I, Row], board_row(Id, I, Row), L),
-	sort(L, LS)
-.
 
 :- dynamic board_row/3.
 :- dynamic board_empty/3.
@@ -175,9 +241,9 @@ board_set_visited(I) :-
 .
 
 board_start_play_dsf(Id) :-
-    Depth = 4,
-	D = root,
-	board_play_dfs(Id, Depth, D)
+    empty_priority_queue(EmptyHeap),
+    enqueue_board(EmptyHeap, Id, 'root', InitialHeap),
+	board_play_dfs(InitialHeap)
 .
 
 board_check(Id):-
@@ -186,34 +252,44 @@ board_check(Id):-
 	board_to(IdG, G),
 	L = G
 .
-board_play_dfs(Id, Depth, _) :-
-   board_check(Id),
-   format('~nEntontrado en ~q!!!!! ~n', [Depth]),
-   board_show(Id)
+board_play_dfs(Heap) :-
+    \+ is_heap_empty(Heap),
+    dequeue_board(Heap, Id, _, _),
+    board_check(Id),
+    format('~nEntontrado ~q!!!!! ~n', [Id]),
+    board_show(Id)
 .
 
-board_play_dfs(Id, Depth, D) :-
-   format('~n(1)Trying board ~a depth=~d move=~s~n', [Id, Depth, D]),
-   Depth > 0, 
-   \+ board_is_visited(Id),
+board_play_dfs(Heap) :-
+    \+ is_heap_empty(Heap),
+    dequeue_board(Heap, Id, Move, RestHeap),
+    priority_calculation(Id, Pri),
+    format('~n(1)Priority ~q ~n', [Pri]),
+    format('~n(1)Trying board ~a move=~s~n', [Id, Move]),
+    \+ board_is_visited(Id),
    board_set_visited(Id),
    findall([IdChild, DChild], board_child(Id, IdChild, DChild), ChildList),
-   Depth1 is Depth - 1,
-   forall(member([IdC, DC], ChildList), board_play_dfs(IdC, Depth1, DC)),
-   format('~n(1)Processed board ~a depth=~d move=~s~n', [Id, Depth, D])
+   enqueue_children(ChildList, RestHeap, NewHeap),
+   board_play_dfs(NewHeap)
 .
-board_play_dfs(Id, Depth, D):-
-   format('~n(2)Trying board ~a depth=~d move=~s~n', [Id, Depth, D]),
-   board_is_visited(Id),
-   format('~n(2)Visited board ~a depth=~d move=~s~n', [Id, Depth, D])
+
+board_play_dfs(Heap):-
+    \+ is_heap_empty(Heap),
+    dequeue_board(Heap, Id, D, NewHeap),
+    format('~n(2)Trying board ~a move=~s~n', [Id, D]),
+    board_is_visited(Id),
+    format('~n(2)Visited board ~a move=~s~n', [Id, D]),
+    board_play_dfs(NewHeap)
 .
-board_play_dfs(Id, Depth, D):-
-   format('~n(3)Trying board ~a depth=~d move=~s~n', [Id, Depth, D]),
-   Depth = 0,
-   format('~n(3)Depth exceding board ~a depth=~d move=~s~n', [Id, Depth, D])
+
+
+enqueue_children([], Heap, Heap).
+enqueue_children([[IdC, DC] | Rest], Heap, NewHeap):-
+    enqueue_board(Heap, IdC, DC, TempHeap),
+    enqueue_children(Rest, TempHeap, NewHeap)
 .
-   
 test(Id) :-
+    board_clear_all,
 	eliminar_diccionario,
 	inicializar_diccionario,
 	L = [
@@ -224,11 +300,11 @@ test(Id) :-
         [1,2,3],
         [8,empty,4],
         [7,6,5]],
-	board_from(G, IdG),
-	guardar_en_diccionario(resultado, IdG),
 	board_clear(_),
 	board_from(L, Id),
-	board_show(Id)
+	board_show(Id),
+    board_from(G, IdG),
+	guardar_en_diccionario(resultado, IdG)
 .
 
 :-
